f3a2e1baa5d54c59389135f88ae3c8c9efdb4066
osxgirl <osxgirl@mac.com>
2021-07-23 19:50:03 -0400

    //❤️

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://petalsnap.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact
         </a>
       </header>
     </div>
